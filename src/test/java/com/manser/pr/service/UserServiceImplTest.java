48a9c0566d2eb1664c253b97cd4b2ab77aecec4a
Serhii Manyk <manukserg1988@gmail.com>
2025-12-02 19:53:08 +0100

    create updateTest() and saveTest() in UserServiceImplTest

--- src/test/java/com/manser/pr/service/UserServiceImplTest.java
+++ src/test/java/com/manser/pr/service/UserServiceImplTest.java
@@ -4,13 +4,14 @@ import com.manser.pr.dao.UserDao;
 import com.manser.pr.domain.User;
 import com.manser.pr.domain.UserRole;
 import com.manser.pr.service.impl.UserServiceImpl;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.AdditionalAnswers.returnsFirstArg;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
@@ -23,19 +24,6 @@ public class UserServiceImplTest {
 
     private UserServiceImpl userService;
 
-    private static User testUser;
-
-    @BeforeAll
-    public static void prepareTestData() {
-        testUser = new User(
-                1L,
-                "Petro",
-                "petro@mail.com",
-                "12345",
-                "503-808-557",
-                UserRole.PATIENT);
-    }
-
     @BeforeEach
     public void init() {
         userService = new UserServiceImpl(userDao);
@@ -43,8 +31,40 @@ public class UserServiceImplTest {
 
     @Test
     public void updateTest(){
-        when(userDao.getById(any(Long.class))).thenReturn(testUser);
         when(userDao.update(any(User.class))).then(returnsFirstArg());
 
+        User userForUpdate = new User(
+                2L,
+                "Ivan",
+                "ivan@mail.com",
+                "67890",
+                "111-222-333",
+                UserRole.PATIENT);
+
+        User result = userService.update(userForUpdate);
+
+        assertNotNull(result);
+        assertEquals("Ivan", result.getName());
+        assertEquals("ivan@mail.com", result.getEmail());
+        assertEquals("67890", result.getPassword());
     }
+
+    @Test
+    public void saveTest(){
+        when(userDao.save(any(User.class))).thenReturn(10L);
+
+        User userForSave = new User(
+                null,
+                "Ivan",
+                "ivan@mail.com",
+                "67890",
+                "111-222-333",
+                UserRole.PATIENT);
+
+        Long resultId = userService.save(userForSave);
+
+        assertEquals(10L, resultId);
+    }
+
+
 }
