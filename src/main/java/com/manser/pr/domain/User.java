bb992bf637d848e9ab4b62dcd024705d720cb717
Serhii Manyk <manukserg1988@gmail.com>
2025-12-05 20:30:19 +0100

    validation refactoring for messages.properties work

--- src/main/java/com/manser/pr/domain/User.java
+++ src/main/java/com/manser/pr/domain/User.java
@@ -12,32 +12,32 @@ public class User {
     @Column(name = "USER_ID")
     private Long id;
 
-    @NotBlank(message = "Name cannot be empty")
-    @Size(max = 100, message = "Name must be less than 100 characters")
+    @NotBlank(message = "{NotEmpty.user.name}")
+    @Size(max = 100, message = "{Size.user.name}")
     @Column(name = "NAME", length = 100, nullable = false)
     private String name;
 
-    @NotBlank(message = "Email cannot be empty")
-    @Email(message = "Invalid email format")
+    @NotBlank(message = "{NotEmpty.user.email}")
+    @Email(message = "{Email.user.email}")
     @Size(max = 100)
     @Column(name = "EMAIL", length = 100, unique = true)
     private String email;
 
-    @NotBlank(message = "Password cannot be empty")
-    @Size(min = 6, max = 100, message = "Password must be 6–100 characters")
+    @NotBlank(message = "{NotEmpty.user.password}")
+    @Size(min = 6, max = 100, message = "{Size.user.password}")
     @Column(name = "PASSWORD", length = 100, nullable = false)
     private String password;
 
     @Pattern(
             regexp = "^\\+?[0-9]{7,20}$",
-            message = "Phone number must contain only digits and optional +"
+            message = "{Pattern.user.phone}"
     )
     @Column(name = "PHONE_NUMBER", length = 20)
     private String phone;
 
-    @NotNull(message = "User role cannot be null")
+    @NotNull(message = "{NotNull.user.userRole}")
     @Enumerated(EnumType.STRING)
-    @Column(name = "USER-ROLE")
+    @Column(name = "USER_ROLE")
     private UserRole userRole;
 
     public User() {
