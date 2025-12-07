772220e4d0fd89abb4dad56ff9ded70af530edc9
Serhii Manyk <manukserg1988@gmail.com>
2025-12-07 17:00:54 +0100

    work with UserController.

--- src/main/java/com/manser/pr/controller/UserController.java
+++ src/main/java/com/manser/pr/controller/UserController.java
@@ -4,9 +4,13 @@ import com.manser.pr.domain.User;
 import com.manser.pr.service.UserService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-import java.util.List;
+import javax.validation.Valid;
 
 @Controller
 public class UserController {
@@ -17,10 +21,69 @@ public class UserController {
         this.userService = userService;
     }
 
-    @GetMapping(value = {"/users"})
+    @GetMapping("/users")
     public String listUsers(Model model){
-        List<User> users = userService.getAll();
-        model.addAttribute("users", users);
+
+        model.addAttribute("users", userService.getAll());
         return "userlist";
+    }
+
+    @GetMapping("/newuser")
+    public String newUser (Model model){
+        model.addAttribute("user",new User());
+        model.addAttribute("edit",false);
+        return "registration";
+    }
+
+    @PostMapping("/newuser")
+    public String saveUser(@Valid User user,
+                           BindingResult result,
+                           RedirectAttributes redirectAttributes){
+
+        if (result.hasErrors()) {
+            return "registration";
+        }
+
+        userService.save(user);
+        redirectAttributes.addFlashAttribute("success", "User " + user.getName() + " " + " registered successfully");
+        return "redirect:/registrationsuccess";
+    }
+
+    @GetMapping("/edit-user-{id}")
+    public String editUser(@PathVariable Long id, Model model){
+        User user = userService.getById(id);
+        if (user == null) {
+            return "redirect:/users";
+        }
+        model.addAttribute("user", user);
+        model.addAttribute("edit",true);
+        return "registration";
+    }
+
+    @PostMapping("/edit-user-{id}")
+    public String updateUser(@Valid User user,
+                             BindingResult result,
+                             RedirectAttributes redirectAttributes){
+        if (result.hasErrors()) {
+            return "registration";
+        }
+        userService.update(user);
+        redirectAttributes.addFlashAttribute("success","User " + user.getName() + " " + " updated successfully");
+        return "redirect:/registrationsuccess";
+    }
+
+    @GetMapping("/delete-user-{id}")
+    public String deleteUser(@PathVariable Long id){
+        User user = userService.getById(id);
+
+        if (user != null) {
+            userService.delete(user);
+        }
+        return "redirect:/users";
+    }
+
+    @GetMapping("/registrationsuccess")
+    public String successPage() {
+        return "registrationsuccess";
     }
 }
