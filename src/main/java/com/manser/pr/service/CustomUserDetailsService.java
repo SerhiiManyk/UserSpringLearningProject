fae78873175b1699c7f64c6b0f285cd8ffcfdb0c
Serhii Manyk <manukserg1988@gmail.com>
2026-01-22 20:18:56 +0100

    TOTAL BAD REFACTORING

--- src/main/java/com/manser/pr/service/CustomUserDetailsService.java
+++ src/main/java/com/manser/pr/service/CustomUserDetailsService.java
@@ -9,15 +9,16 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
 
 import java.util.Collections;
 
-@Service
+@Service("customUserDetailsService")
+@Transactional(readOnly = true)
 public class CustomUserDetailsService implements UserDetailsService {
 
-    private UserDao userDao;
-
-    public CustomUserDetailsService() {}
+    private final UserDao userDao;
 
     @Autowired
     public CustomUserDetailsService(UserDao userDao) {
